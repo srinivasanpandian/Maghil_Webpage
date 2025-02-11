dd6499244ad145bbec7dc4baa93ef88a71ea512c
srinivasanpandian <srinivasapandian29@gmail.com>
2025-02-11 15:54:02 +0530

    homepagechange commit

--- script.js
+++ script.js
@@ -17,3 +17,40 @@ productContainers.forEach((item, i) => {
 document.querySelector(".card-btn").addEventListener("click", function () {
     window.location.href = "features.html";
   });
+ 
+
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll(".slide");
+  let dots = document.querySelectorAll(".dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "block";  
+  dots[slideIndex - 1].classList.add("active");
+}
